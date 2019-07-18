444a14c99ead1fa36db87253d9899dabd4e79a5c
cssi <cssi@cssis-MacBook-Pro-3.local>
2019-07-18 07:34:24 -0700

    Added bloghandler but it's unfinished. Continue to debug after

--- main.py
+++ main.py
@@ -11,6 +11,23 @@ class htmlHandler(webapp2.RequestHandler):
     def get(self):
         template = jinja_env.get_template('templates/index.html')
         self.response.write(template.render())
+
+
+
+class BlogHandler(webapp2.RequestHandler):
+    def get(self):
+        template = jinja_env.get_template('templates/new_post.html')
+        self.response.write(template.render())
+    def post(self):
+        title_var = self.request.get('Title')
+        content_var = self.request.get('Content')
+        author_var = self.request.get('AuthorName')
+        print(title_var)
+        print(content_var)
+        print(author_var)
+        self.response.write('good job')
+
 app = webapp2.WSGIApplication([
     ('/', htmlHandler),
-], debug=True)
+    ('/Blog'),
+], debug = True)
